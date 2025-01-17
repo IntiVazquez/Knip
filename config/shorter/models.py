1baeada424cf0c8050113b00b35a88ffc4cf025a
IntiVazquez <inti.vazquez.musso@gmail.com>
2025-01-17 18:22:16 -0300

    Cambio Url de la pagina

--- config/shorter/models.py
+++ config/shorter/models.py
@@ -34,7 +34,7 @@ class URL(models.Model):
 
 
     def __str__(self):
-        page_url = "http://127.0.0.1:8000"
+        page_url = "knip.vercel.app"
         return f"{page_url}/{self.short_url}"
 
     def get_creation_date(self):
