505eee5c13a495286678817b83830396e45aa89c
IntiVazquez <inti.vazquez.musso@gmail.com>
2025-01-18 00:19:21 -0300

    Test staticfiles

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,6 @@ from django.urls import include, path
 from config.dashboard.views import DashboardView 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("accounts/", include('allauth.urls')),
     
     path("dashboard/", include('config.dashboard.urls')),
